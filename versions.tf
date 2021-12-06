b5c8ff16070c10b0bf19f7ecc26cd37dae4b94d5
Gene Leyfer <gleyfer@aviatrix.com>
2021-12-06 10:46:24 -0500

    Fix providers

--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.25"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.72"
-    }
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
       version = "~> 2.20"
